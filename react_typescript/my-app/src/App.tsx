340f4e99cee9f92c093aeadfddaa1ad5d0c10b7a
Srutip04 <subuhishruti75@gmail.com>
2023-01-21 18:24:11 +0530

    adding data models

--- react_typescript/my-app/src/App.tsx
+++ react_typescript/my-app/src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import Todos from './components/Todos';
+import Todo from './models/todo';
 import './App.css';
 
 function App() {
+  const todos =[
+    new Todo('Learn React'),
+    new Todo('Learn Typescript')
+  ]
   return (
     <div className="App">
-     <Todos items={['Learn react','learn typescript']}/>
+     <Todos items={todos}/>
     </div>
   );
 }
