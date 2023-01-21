7a1a7f079cbd1b140cd2d9c54c0c411d85678da9
Srutip04 <subuhishruti75@gmail.com>
2023-01-21 11:35:20 +0530

    base types

---
+++ ts-basics/basics.ts
@@ -0,0 +1,14 @@
+//primitives: number,string,boolean
+//more complex types: arrays,objects
+//function types,parameters
+
+//Primitives
+
+let age: number;
+age = 12;
+
+let userName: string;
+userName = "Max";
+
+let isIns : boolean;
+isIns =true;
